4c4fe0e5220cd1eae2c563e89f5d1e43e2c3f470
Pedro Albano <phmello8@gmail.com>
2023-08-03 14:28:07 -0300

    Project cleaning
    Removing excess assets and adding new textures and shaders

--- Assets/_Project/Scripts/CursorHandler.cs
+++ Assets/_Project/Scripts/CursorHandler.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Tilemaps;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class CursorHandler : MonoBehaviour
 {
